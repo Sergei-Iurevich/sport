5f1177c0bc35c9bf4d78588a7312d892a39e000a
Sergei Iurevich <lask2su@gmail.com>
2021-03-19 13:08:02 +0300

    Set up devserver

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,10 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'index_bundle.js',
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
     plugins: [new HtmlWebpackPlugin()],
 };
