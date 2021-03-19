9887facf953170d099c969106a6c620e919afce1
Sergei Iurevich <lask2su@gmail.com>
2021-03-19 18:46:15 +0300

    Set up stylelint

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
-    mode: "production",
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index_bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                // https://webpack.js.org/loaders/css-loader/
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new BrowserSyncPlugin(
-            {
-                // browse to http://localhost:3000/ during development
-                host: "localhost",
-                port: 3000,
-                // proxy the Webpack Dev Server endpoint
-                // (which should be serving on http://localhost:3100/)
-                // through BrowserSync
-                proxy: "http://localhost:9000/",
-            })
-
-
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        // https://webpack.js.org/loaders/css-loader/
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development
+      host: "localhost",
+      port: 3000,
+      // proxy the Webpack Dev Server endpoint
+      // (which should be serving on http://localhost:3100/)
+      // through BrowserSync
+      proxy: "http://localhost:9000/",
+    }),
+  ],
 };
